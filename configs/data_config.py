8e4ccd61a3cc99013fa0252128eafa67f3563d52
MariaPdg <mpodguzova25@gmail.com>
2021-04-07 14:55:49 +0200

    add docs

--- configs/data_config.py
+++ configs/data_config.py
@@ -1,7 +1,11 @@
 
+# root where datasets are located (might be home directory)
 data_root = 'datasets/'
+
+# data split
 coco_train_data = 'coco/coco_train2017/train2017'
 coco_valid_data = 'coco/coco_valid2017/val2017'
 coco_test_data = 'coco/coco_test2017/test2017'
 
+# path where save the results
 save_training_results = 'results/'
